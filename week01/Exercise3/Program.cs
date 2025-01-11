905d47bbf54b8df1eae96634f22c13e64fae128a
heathannikins <heatheranni28@gmail.com>
2025-01-11 08:42:16 -0700

    Got the Core requirement, but struggling on the stretch exercises

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "Yes";
+
+        while (playAgain == "Yes")
+        {
+            int count = 0;
+
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+            Console.WriteLine(magicNumber);
+
+
+            int guess = 0;
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                count = count +1;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.Write("Do you want to play again? ");
+//                    playAgain = Console.ReadLine();
+                }
+//                Console.Write("Do you want to play again? ");
+            }
+//        Console.Write("Do you want to play again? ");
+        }
     }
 }
