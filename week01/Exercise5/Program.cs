a014b0eb61be2fdf0dcbec2e35c4f3f7a0800332
heathannikins <heatheranni28@gmail.com>
2025-01-11 20:46:27 -0700

    Finished Exercise 1/11/2025

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        {
+            DisplayMessage();
+
+            string userName = UserName();
+            int userNumber = UserNumber();
+
+            int squaredNumber = SquareRootOfNumber(userNumber);
+
+            DisplayResults(userName, squaredNumber);
+
+        }
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string UserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int UserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        static int SquareRootOfNumber(int number)
+        {
+            int squareRoot = number * number;
+            return squareRoot;
+        }
+
+        static void DisplayResults(string name, int squared)
+        {
+            Console.WriteLine($"{name}, the square root of your number is {squared}.");
+        }
+
+
+
+
     }
 }
