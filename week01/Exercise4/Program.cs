c3089b454357cbccc5d3c9d8cb3595b3e5fd5374
heathannikins <heatheranni28@gmail.com>
2025-01-11 19:54:18 -0700

    Finished Exercise 4 - 1/11/2025

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> listOfNumbers = new List<int>();
+
+        int number = -1;
+        float total = 0;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        while (number != 0)
+        {
+            Console.Write("Enter number ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                listOfNumbers.Add(number);
+            }
+        }
+        foreach (int numb in listOfNumbers)
+        {
+            total += numb;
+        }
+        Console.WriteLine($"The sum is {total}");
+        float average = total / (listOfNumbers.Count);
+        Console.WriteLine($"The average is {average}");
+        int largest = listOfNumbers.Max();
+        Console.WriteLine($"The largest number is {largest}");
+        int smallest = listOfNumbers.Min();
+        Console.WriteLine($"The smallest number is {smallest}");
     }
 }
