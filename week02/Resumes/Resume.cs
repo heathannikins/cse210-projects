ce56dac6ac4d306b70361afeffc77ed8da19aa24
heathannikins <heatheranni28@gmail.com>
2025-01-14 20:26:51 -0700

    Finish W02 Learning Activity Abstraction

---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,18 @@
+// New Resume class
+
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public void DisplayJob()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs: ");
+        foreach (Job j in _jobs)
+        {
+            j.DisplayDetails();
+        }
+    }
+
+}
